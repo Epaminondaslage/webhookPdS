58cc053328be41e66008b7eb6c9715da33512f4c
Epaminondas Lage <epaminondaslage@gmail.com>
2024-11-19 07:21:39 -0300

    update

--- public/horario/config_horario.php
+++ public/horario/config_horario.php
@@ -1,7 +1,10 @@
 <?php
 // config/config.php
-define('MQTT_SERVER', '10.0.0.32');
-define('MQTT_PORT', 1883);
-define('MQTT_USERNAME', 'mqtt');
-define('MQTT_PASSWORD', 'planeta');
-define('MQTT_TOPIC', 'horario_alarm');
+return [
+    'server' => '10.0.0.32',
+    'port' => 1883,
+    'username' => 'mqtt',
+    'password' => 'planeta',
+    'client_id' => 'ClientID_' . rand(),
+    'topic' => 'horario_alarm',
+];
--- public/horario/mqtt_client_horario.php
+++ public/horario/mqtt_client_horario.php
@@ -1,8 +1,32 @@
 <?php
-// public/mqtt_client.php
-require_once("../src/MqttClient.php");
+// src/MqttClient.php
+require_once '../config/config.php';
+require_once '../vendor/autoload.php';  // Certifique-se de que este caminho está correto
 
-$message = $argv[1] ?? 'default_message';
+class MqttClient {
+    private $server;
+    private $port;
+    private $username;
+    private $password;
+    private $topic;
 
-$mqttClient = new MqttClient();
-echo $mqttClient->publish($message);
+    public function __construct() {
+        $this->server = MQTT_SERVER;
+        $this->port = MQTT_PORT;
+        $this->username = MQTT_USERNAME;
+        $this->password = MQTT_PASSWORD;
+        $this->topic = MQTT_TOPIC;
+    }
+
+    public function publish($message) {
+        $mqtt = new phpMQTT($this->server, $this->port, "ClientID" . rand());
+
+        if ($mqtt->connect(true, NULL, $this->username, $this->password)) {
+            $mqtt->publish($this->topic, $message, 0);
+            $mqtt->close();
+            return "Mensagem enviada com sucesso.";
+        } else {
+            return "Erro ao conectar ao broker MQTT.";
+        }
+    }
+}
